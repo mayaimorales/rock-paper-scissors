a5a619b2adc3a79edd37e73c087b6512ec4f2ee6
Maya Morales <mayainomorales@gmail.com>
2022-03-30 12:29:07 -0700

    add window alerts

---
+++ script.js
@@ -0,0 +1,16 @@
+//First thing we want to do is load a window confirm box asking the user if they want to play.
+
+if (window.confirm("Do you want to play?")){
+    window.alert("You hit OK");
+} else {
+    window.alert("You Canceled");
+}
+
+//If user hits OK the game play will start. If user hits cancel, "You Canceled" will display
+
+var input = window.prompt("Rock, Paper, or Scissors?")
+   
+
+
+
+
